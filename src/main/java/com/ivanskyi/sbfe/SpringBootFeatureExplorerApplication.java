c5b8f52a45cfd310a373ce48bb5ee78efbb7ef9b
ivanskyi <olehfromukraine@gmail.com>
2025-06-23 20:14:26 +0300

    Created project

---
+++ src/main/java/com/ivanskyi/sbfe/SpringBootFeatureExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.ivanskyi.sbfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFeatureExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFeatureExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanskyi/sbfe/SpringBootFeatureExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanskyi.sbfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeatureExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
